ac40f501a3e0ddbd12e5441bc0cc7dd650caa921
Ajay Singh <ajaykripa8736968359@gmail.com>
2026-02-28 20:54:31 +0530

    fix ruby linting
    Signed-off-by: Ajay Singh <ajaykripa8736968359@gmail.com>

--- app/mailers/survey_mailer.rb
+++ app/mailers/survey_mailer.rb
@@ -88,7 +88,7 @@ class SurveyMailer < ApplicationMailer
   end
 
   def paragraphify(text)
-    # Handles both Windows and Unix line breaks, and splits on blank lines (i.e. two consecutive line breaks)
+    # Handles both Windows and Unix line breaks
     text.split(/\r?\n\r?\n/)
   end
 end
